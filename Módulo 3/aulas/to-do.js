ca3ef0cfa4d42c968229db0dd2fbaf02aa3c5840
Lais Escorcio <lais.correia@outlook.com>
2020-04-18 13:09:25 -0300

    include files

---
+++ Módulo 3/aulas/to-do.js
@@ -0,0 +1,8 @@
+// Primeiro referencia a lista (ul)
+var listElement = document.querySelector("#app ul");
+// Referencia o input
+var inputElement = document.querySelector("#app input");
+// Referencia o botão
+var buttonElement = documento.querySelector("#app button");
+
+// Os itens no html estão estáticos, mas eles precisam ser dinamicos para excluir, adicionar etc
---
+++ Módulo 3/aulas/to-do2.js
@@ -0,0 +1,22 @@
+// Primeiro referencia a lista (ul)
+var listElement = document.querySelector("#app ul");
+// Referencia o input
+var inputElement = document.querySelector("#app input");
+// Referencia o botão
+var buttonElement = documento.querySelector("#app button");
+
+// Os itens no html estão estáticos, mas eles precisam ser dinamicos para excluir, adicionar etc
+var todos = [
+  "Fazer café",
+  "Estudar Javascript",
+  "Acessar comunidade da Rocketseat",
+  // Se essa estrutura fosse mais complexa, poderia ser em foma de array de objetos, portanto, pra cada to-do um objeto
+  //Exemplo: var todos = [{text: 'Fazer café', propriedades: 1}]
+];
+
+//Para renderizar na tela os itens to-do
+// Coloca no JS porque futuramente que precisar excluir ou adicionar itens, já tem uma funçao pra isso
+function renderTodos() {
+  // For específico pra arrays
+  for ()
+}
